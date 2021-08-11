66a1ee419ce6d619af4f8dd4cd0a80bbca122c42
tommytommychopper <t.itsuki928@gmail.com>
2021-08-11 03:24:34 -0700

    [add] get and set of storage option func

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,7 +1,11 @@
 export interface LocalStorage {
 	words?: string[]
+	option?: LocalStorageOption
 }
-
+export type WordLanguage = 'jp' | 'en'
+export interface LocalStorageOption {
+	lang?: WordLanguage
+}
 export type LocalStorageKey = keyof LocalStorage
 
 export function getStoredWords(): Promise<string[]> {
@@ -15,6 +19,24 @@ export function getStoredWords(): Promise<string[]> {
 
 export function setStoredWords(words: string[]): Promise<void> {
 	const vals: LocalStorage = {words: words}
+	return new Promise((resolve) => {
+		chrome.storage.local.set(vals, () => {
+			resolve()
+		})
+	})
+}
+
+export function getStoredOption(): Promise<LocalStorageOption> {
+	const key: LocalStorageKey[] = ['option']
+	return new Promise((resolve) => {
+		chrome.storage.local.get(key, (res: LocalStorage) => {
+			resolve(res.option)
+		})
+	})
+}
+
+export function setStoredOption(option: LocalStorageOption): Promise<void> {
+	const vals: LocalStorage = {option: option}
 	return new Promise((resolve) => {
 		chrome.storage.local.set(vals, () => {
 			resolve()
